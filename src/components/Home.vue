184a2d8b18368a9a535b35a06aebdac73b9667d9
Imthealex <alextwirih@gmail.com>
2024-07-13 13:06:27 +0300

    Alex Project

---
+++ src/components/Home.vue
@@ -0,0 +1,266 @@
+<template>
+  <div id="Home">
+    <div id="Hom">
+    <div class="HM">
+      <div class="header">
+        <l>
+    Secure Delivery and special offers | Need help?+254 743 946 228 </l>
+      </div>
+    <div style="background-color: rgba(22, 22, 22); float: right;">
+      <strong></strong>
+
+      <v-spacer ></v-spacer>
+<div ><v-btn
+        v-for="icon in icons"
+        :key="icon"
+        class="mx-4"
+        :icon="icon"
+        variant="plain"
+        size="small"
+        style="color: goldenrod;  float: right; "
+      ></v-btn></div></div>
+      <div class="Nav" >
+   
+
+   <v-btn to="/Home" style="background:black; color: goldenrod;font-family: Platypi, serif;margin-right: 5px; font-weight: 200; margin-left: 50px; font-size: 18px;">HOME</v-btn>
+   
+   <v-btn href="http://localhost:5173/Product" style="background:black; color: goldenrod; font-family: Platypi, serif; font-weight: 200; font-size:18px;">
+    PRODUCT</v-btn>  
+   
+   <v-btn value="account" href=""   
+   style="background:black; color: goldenrod; margin-left:30px;">
+         <v-icon>mdi-account</v-icon>
+        
+       </v-btn>
+     
+    <v-btn value="cart" href=""  style="background:black; color: goldenrod;">
+         <v-icon>mdi-cart</v-icon>
+       </v-btn></div>
+<div class="Til">
+  <H style=" font-family: Lemon, serif; font-weight: 700; font-size:70px; color: silver;">Silver Side</H>
+</div>
+<div class="pl">
+  <p style=" font-family: Lemon, serif; font-weight: 100; font-size:30px; color: silver;text-align: center;"> is a large, lean, boneless cut of meat with a course grained texture. It is mostly used for roasting joints, braising steaks or dice.</p>
+</div>
+<div class="shop">
+  <v-btn href="http://localhost:5173/Product" elevation="16" size="x-large" style="background: silver; font-family: Lemon, serif; font-weight: 600; margin-right: 30px;"
+          >Shop Products</v-btn>
+
+  <v-btn href="http://localhost:5173/Product" elevation="16" size="x-large" style="background: silver; font-family: I nika, serif; font-weight: 600; margin-left: 30px;"
+          >Shop Silverside</v-btn>
+</div>  
+</div>
+</div>
+<div class="img2">
+  <div class="Hm">
+    <h0 style="text-decoration: underline;color: aliceblue; font-size: 70px; position: relative;
+   left:42%; top:80px; font-family: Lemon, serif;">Special<k style="color: goldenrod;">Rates</k></h0> 
+    <v-img
+  src="https://stanfords.com/wp-content/uploads/2022/02/louis-hansel-oyUqUV1Q0Zg-unsplash-1.jpg"
+  height="550"
+  width="460"
+  style="position:relative; top: 20%; left:5%; "
+  cover
+></v-img>
+<div class="Special">
+ 
+<m class="ul">
+  <br>
+<d>Rump Steak ___________ sh 750</d>
+<br>
+<br>
+<d>Chicken ______________ sh 500</d>
+<br>
+<br>
+<d>Tilapia _______________ sh 600</d>
+<br>
+<br>
+<d>Drum sticks ___________ sh 690</d>
+<br>
+<br>
+<d>Silver side ____________ sh 750</d>
+</m>
+</div>
+  </div>
+  </div>
+      <div class="Ab">
+        <v-img
+  src="https://images.squarespace-cdn.com/content/v1/5ed25ead6533665d5bd7dd4c/1591804647760-AH3YHZ40AGCBCXNH00F0/victoria-shes-XgFFJKSPkNk-unsplash.jpg"
+  height="600"
+  width="500"
+  style="position: relative; top:30%; left:5%;"
+  cover
+></v-img>
+<div class="stat">
+  <Strong>
+  <z style="font-family: Lemon, serif; font-size: 36px; color: red;">About Us</z><br><br>
+  <f style="font-size: 96px; font-family: Lemon, serif;">About Our Firm</f><br><br>
+  <x><n>Tekeran Butchery </n>is a rising giant that mainly focuses <br>on <n>providing quality and consistency
+ in maintenance<br> of the products</n> and we also 
+focus on the <br>customers needs.</x>
+</Strong>
+</div>
+<br>
+<div class="icons">
+  <strong style="color: aliceblue; margin-right: 200px;"><v-icon style="color: goldenrod;">mdi-car</v-icon> Delivery</strong>       
+<strong style="color: aliceblue;"><v-icon style="color: goldenrod;">mdi-food-drumstick</v-icon> Fresh meat</strong>
+<br>
+<br>
+<strong style="color: aliceblue; margin-right: 120px;"><v-icon style="color: goldenrod;">mdi-cash</v-icon> Amazing Deals</strong>
+<strong style="color: aliceblue;"><v-icon style="color: goldenrod;">mdi-face-agent</v-icon> Available</strong>
+</div>
+      </div>
+      <div class="Now">
+        <strong style="color: goldenrod; font-size: 36px; position:relative; top:30px; left:48%; font-family: Lemon, serif;">Shop Now!!</strong>
+        <br><br>
+        <strong style="color: aliceblue;font-size: 48px; position:relative; top:30px; left:40%; font-family: Lemon, serif;">Our Quality Products</strong>
+        <v-img
+  src="https://st.depositphotos.com/1007995/57407/i/450/depositphotos_574076038-stock-photo-food-concept-uncooked-red-meat.jpg"
+  height="420"
+  width="420"
+  style="position:relative; top:10%; left:3%; border-radius: 30px; background-color: black; opacity: 60%;"
+  cover
+><strong href="http://127.0.0.1:5500/Tekeran-Project/Product.html" style="font-size:48px; color: white; font-family: Lemon, serif; position: relative; top: 40%; left:40%;">Beef</strong></v-img><v-img
+  src="https://www.newpi.coop/application/files/8116/4858/4137/chicken-bundle_sq.jpg"
+  height="420"
+  width="420"
+  style="position:relative;  left: 35%; bottom:38%; border-radius: 30px; background-color: black; opacity: 60%;"
+  cover
+><strong style="font-size:48px; color: white; font-family: Lemon, serif; position: relative; top: 38%; left:32%;">Chicken</strong>
+</v-img>
+<v-img
+  src="https://zonefresh.com.au/wp-content/uploads/SPARE-RIBS.jpg"
+  height="420"
+  width="420"
+  style="position:relative; left:67%; bottom: 85%; border-radius: 30px; background-color: black; opacity: 60%;"
+  cover
+><strong style="font-size:48px; color: white; font-family: Lemon, serif; position: relative; top: 40%; left:40%;">Goat</strong></v-img>
+
+      </div>
+</div>  
+</template>
+<style>
+#Home{
+  background-color: darkred;
+}
+#Hom{
+background-image:url("https://img.freepik.com/free-photo/pieces-raw-fresh-meat-isolated-black-stone-board-mirrored-side-view_346278-817.jpg") ;
+  height:120vh;
+  width:100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+.img2{
+background-color: black;
+background-image: url("https://res.cloudinary.com/the-infatuation/image/upload/v1657763082/cms/guides/dark-chicago-restaurant-power-rankings/TangoSur_sandynoto_2.jpg");
+height:180vh;
+  width:100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+.Now{
+  background-color: rgba(22, 22, 22);
+  height:120vh;
+  width:100%;
+}
+.Ab{
+  background-color: rgb(14, 14, 14);
+  height:180vh;
+  width:100%;
+}
+.HM{
+background-color: black;
+background-position: center;
+  background-size: cover;
+  height:120vh;
+  width:100%;
+  opacity:80%;
+}
+.Hm{
+  background-color: black;
+background-position: center;
+  background-size: cover;
+  height:200vh;
+  width:100%;
+  opacity:70%;
+}
+.header{
+  background-color: rgba(22, 22, 22);text-align:center;float: left;margin-left: 35%;border-bottom-left-radius: 20px;border-bottom-right-radius: 20px; padding: 8px;
+  font-family: "Imprima", sans-serif;
+  color: goldenrod;
+  opacity:100%;
+}
+.Nav{
+position:absolute;
+left:70%;
+top:70px;
+}
+.Til{
+position: absolute;
+left:40%;
+top: 300px;
+}
+.pl{
+  position: absolute;
+left:45%;
+top: 400px;
+transform: translate(-45%);
+}
+.shop{
+  position: absolute;
+left:48%;
+top: 700px;
+transform: translate(-48%);
+}
+
+.ul{
+color: aliceblue;
+font-family: "Bitter", serif;
+font-size: 40px;
+position: relative;
+left:46%;
+bottom:300px;
+}
+
+.stat{
+  color: aliceblue; 
+  position:relative;
+  left:42%;
+  bottom:20%;
+}
+.icons{
+  position:relative;
+  left:42%;
+  bottom:19%;
+  font-size: 30px;
+  font-family: "Bitter", serif;
+}
+x{
+  font-family: "Bitter", serif;
+ font-size: 36px;
+ align-items: justify;
+}
+n{
+  color: goldenrod;
+}
+</style>
+<script>
+
+const variants = ['elevated', 'flat', 'tonal', 'outlined']
+
+import Home from "./Home.vue";
+
+
+export default {
+  data: () => ({
+    icons: [
+      'mdi-facebook',
+      'mdi-twitter',
+      'mdi-linkedin',
+      'mdi-instagram',
+    ],
+  }),
+}
+</script>
